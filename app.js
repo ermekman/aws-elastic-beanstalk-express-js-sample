76a7213bee4c7587cb3e60e4a0397a544c851740
ermek djunushev <ermekman@gmail.com>
2024-01-08 12:50:02 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I am mastering DevOps, taking my learning journey yet to another level!'));
+app.get('/', (req, res) => res.send('I am mastering DevOps, taking my learning journey yet to another level! This time around, I am making code changes to be built on CodeBuild and deploy via ElasticBeanstalk'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+
+
